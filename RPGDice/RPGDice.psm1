6381d9b0df4650b203ca4135adee606f8e0c16be
Gilbert Sanchez <gsanchez@fb.com>
2021-09-05 12:37:24 -0700

    Remove private folder and update module metadata

--- RPGDice/RPGDice.psm1
+++ RPGDice/RPGDice.psm1
@@ -2,8 +2,7 @@
 $enums = @(Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath 'Enums/*.ps1') -Recurse -ErrorAction Stop)
 $classes = @(Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath 'Classes/*.ps1') -Recurse -ErrorAction Stop)
 $public = @(Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath 'Public/*.ps1')  -Recurse -ErrorAction Stop)
-$private = @(Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath 'Private/*.ps1') -Recurse -ErrorAction Stop)
-foreach ($import in @($enums + $classes + $public + $private)) {
+foreach ($import in @($enums + $classes + $public)) {
     try {
         . $import.FullName
     } catch {
